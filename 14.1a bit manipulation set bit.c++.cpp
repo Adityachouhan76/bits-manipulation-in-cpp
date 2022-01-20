7b2bae0bca68bb87c910b7017086bd457366dba2
ADITYA CHOUHAN <90592325+Adityachouhan76@users.noreply.github.com>
2022-01-20 14:09:08 +0530

    Add files via upload

---
+++ 14.1a bit manipulation set bit.c++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int get(int n, int pos){
+	return((n & (1<<pos))!=0);
+}
+
+int setBit(int n, int pos){
+	return (n | (1<<pos));
+}
+
+int main(){
+	cout<<setBit(5,1)<<endl;
+	
+	return 0;
+}
+
